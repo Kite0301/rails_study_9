82dfbb025451088ba1a8fb3317219c96cc56cdc2
Kite0301 <kaito.titech@gmail.com>
2017-05-29 12:36:59 +0900

    Page 3
    Set user_id when create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,23 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
-  
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find_by(id: params[:id])
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id,
+    )
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
@@ -22,11 +25,11 @@ class PostsController < ApplicationController
       render("posts/new")
     end
   end
-  
+
   def edit
     @post = Post.find_by(id: params[:id])
   end
-  
+
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
@@ -37,12 +40,12 @@ class PostsController < ApplicationController
       render("posts/edit")
     end
   end
-  
+
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
-  
+
 end
